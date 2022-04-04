6972ba5411373ff5469a0c4aabd74c387fffb517
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2022-04-04 20:30:31 +0200

    hm idk a cool effect / particle for egg :/

--- src/ryzerbe/easter/scheduler/UpdateScheduler.php
+++ src/ryzerbe/easter/scheduler/UpdateScheduler.php
@@ -2,18 +2,28 @@
 declare(strict_types=1);
 namespace ryzerbe\easter\scheduler;
 
+use javamapconverter\entity\SkullEntity;
 use pocketmine\scheduler\Task;
+use pocketmine\Server;
 use ryzerbe\easter\session\PlayerSessionManager;
 
 
 class UpdateScheduler extends Task{
 	public function onRun(int $currentTick){
+		if($currentTick % 60 === 0) {
+			$level = Server::getInstance()->getDefaultLevel();
+			foreach ($level->getEntities() as $entity) {
+				if(!$entity instanceof SkullEntity) continue;
+				$position = $entity->asPosition();
+
+				//TODO: ADD COOL EFFECT / PARTICLE TO THE EGG
+			}
+		}
+
 		if($currentTick % 20 !== 0) return;
 
 		foreach (PlayerSessionManager::getSessions() as $playerSession) {
 			$playerSession->onUpdate($currentTick);
 		}
-
-		//TODO: Spawn cool particle about the egg
 	}
 }
